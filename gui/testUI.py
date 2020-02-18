583b7cfd68a1be731417d76677c176e730ac51fd
bdemin <42113340+bdemin@users.noreply.github.com>
2020-02-18 21:30:10 +0200

    Update terrain  values on combobox event

--- gui/testUI.py
+++ gui/testUI.py
@@ -11,9 +11,16 @@ class MyQtApp(Ui_MainWindow):
 
         self.comboBoxVehicleType.textHighlighted.connect(self.on_vehicle_type_change)
         self.comboBoxVehicleType.currentTextChanged.connect(self.on_vehicle_type_change)
+        self.comboBoxTerrainTypeOptions.currentTextChanged.connect(self.on_terrain_type_change)
+
     def on_vehicle_type_change(self, val):
         image = ":/Vehicles/" + val + ".jpg"
         self.labelVehicleImg.setPixmap(QtGui.QPixmap(image))
+
+
+    def on_terrain_type_change(self, val):
+        values = self.terrain_types[val] # list
+        self.labelTerrainParams.setText(f"\u03BC = {values[0]} |  Ci = {values[1]} | Ri = {values[2]}")
 
     def set_terrain_types(self):
         self.terrain_types = {'LETE Sand': [1,2,3],
