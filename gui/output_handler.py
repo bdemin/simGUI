8219608cfab3fff2791967988a2e4d5ed8e18f67
bdemin <42113340+bdemin@users.noreply.github.com>
2020-03-09 20:28:35 +0200

    Created output handler for Eitan vehicle

---
+++ gui/output_handler.py
@@ -0,0 +1,23 @@
+def get_output(qt_app):
+    out_dict = {}
+
+    out_dict['vehicleName'] = qt_app.comboBoxVehicleType.currentText()
+    out_dict['vehicleType'] = get_vehicle_type(out_dict['vehicleName'])
+
+    out_dict['differential'] = qt_app.checkBoxDifferential.isChecked()
+    
+    index = qt_app.comboBoxTerrainTypeOptions.currentText()
+    out_dict['terrainProfileType'] = qt_app.terrain_types[index]
+    
+    out_dict['terrainSlopeAngle'] = qt_app.spinBoxSlope.value()
+
+    out_dict['terrainGroundType'] = qt_app.comboBoxTerrainTypeOptions.currentText()
+
+    out_dict['terrainMu'] = qt_app.terrain_types[index]
+
+
+
+def get_vehicle_type(vehicle):
+    if vehicle in ['M113', 'MK4', 'Namer']:
+        return 'Wheeled'
+    return 'Tracked'
