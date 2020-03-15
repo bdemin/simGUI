43a411558d2a1a506a304250609caecf2f93ced8
bdemin <42113340+bdemin@users.noreply.github.com>
2020-03-15 21:21:09 +0200

    Update sim_test.m

--- matlab_code_test/sim_test.m
+++ matlab_code_test/sim_test.m
@@ -1,10 +1,14 @@
 
-function a = sim_test(input)
-disp(input)
+function sim_test()
+
 for i = 1:100000000
     if mod(i, 10000000) == 0
         disp(i)
     end
 end
-a = i;
+
+x = linspace(0, 4*pi);
+y = sin(x);
+plot(x,y)
+
 end
