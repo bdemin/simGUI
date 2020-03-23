78fad360f722c09066aa6f07b984b706e4daea7e
bdemin <42113340+bdemin@users.noreply.github.com>
2020-03-23 21:29:04 +0200

    Removed all matlab.engine use cases

--- gui/simulation_trigger.py
+++ gui/simulation_trigger.py
@@ -1,44 +1,5 @@
 from os import getcwd, chdir
 
-import matlab.engine
-
-
-def run():
-    print('Matlab Engine is running\n')
-
-    cwd = getcwd()
-    chdir(cwd + "\\matlab_code_test")
-
-    eng = matlab.engine.start_matlab()
-    output = eng.sim_test('input_args')
-    print('Matlab output: ', output)
-
-    print('Done')
-
-
-def run_m113():
-    print('Running M113 Simulation\n')
-
-    cwd = getcwd()
-    chdir(cwd + "\\..\\M113_tests")
-
-    eng = matlab.engine.start_matlab()
-    eng.M113_MainFile(nargout = 0)
-
-    print('Done')
-
-def run_async(tabWidget):
-    cwd = getcwd()
-    chdir(cwd + "\\matlab_code_test")
-
-    eng = matlab.engine.start_matlab()
-
-    import io
-    out = io.StringIO()
-    ret = eng.sim_test('input_args', background=True, stdout=out)
-    print(out.getvalue())
-    print(ret)
-    tabWidget.setDisabled(False)
 
 def run_exec(vehicle):
     import subprocess
